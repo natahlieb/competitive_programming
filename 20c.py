b6177b0176bde94b2e3c91e9df0c24a4a7dff827
Natahlie <natahliebeavers@gmail.com>
2015-09-16 12:34:41 -0500

    almost through with implementation for 20c

--- 20c.py
+++ 20c.py
@@ -1,28 +1,30 @@
 __author__ = 'natahlie'
 import numpy as np
+import sys
 
-
-def find_shortest_distance(adj_matrix, origin_node, n):
+def find_shortest_distance(dist, visited_set, n):
+    shortest_distance = sys.maxint
     count = 0
-    shortest_distance = 10000
     shortest_distance_node = -1
+
     while(count < n):
-        if adj_matrix[origin_node][count]<shortest_distance:
-            shortest_distance = adj_matrix[origin_node][count]
-            shortest_distance_node = (count+1)
+        if visited_set[count]== False & dist[count] <= shortest_distance:
+            shortest_distance = dist[count]
+            shortest_distance_node = count
+
         count +=1
 
     return shortest_distance_node
 
 
 
-
+#construct our adjacency matrix
 first_line = raw_input().split(" ")
 n = int(first_line[0])
 m = int(first_line[1])
 print n
 adj_matrix = np.empty((n, n))
-adj_matrix.fill(100000)
+adj_matrix.fill(sys.maxint)
 print adj_matrix
 
 i = 0
@@ -32,62 +34,35 @@ while i != m:
     adj_matrix[int(temp_input[1])-1, int(temp_input[0])-1] = int(temp_input[2])
     i+=1
 
-origin = 1
-goal = n
+origin = 0
+goal = n-1
 
 dist = np.empty(n)
-dist[origin-1] = 0
-prev = np.empty(n)
-prev[origin] = -1
+shortest_set = np.empty(n)
+count = 0
 
-nodes_not_visited = []
-
-count = 1
-nodes_not_visited.append(1)
-
-while(count < n-1):
-    dist[count] = 10000
-    prev[count] = -1
-    nodes_not_visited.append(count+1)
+while(count < n):
+    dist[count] =  sys.maxint
+    shortest_set = 'FALSE'
     count+=1
 
+dist[origin] =0
 
-print nodes_not_visited
-print dist
-print count
+i = 0
 
-while len(nodes_not_visited)!=0:
-    shortest_distance
+while(i < n):
+    #find min distance vertex from those not yet processed
+    min_node = find_shortest_distance(dist, shortest_set,n)
+    shortest_set[min_node] = True
+    j = 0
+    while(j < n):
+        if(shortest_set[j]==False & adj_matrix[min_node][j])!=sys.maxint & dist[min_node]+adj_matrix[min_node][j] < dist[i]:
+            dist[i] = dist[min_node] + adj_matrix[i][j]
+        j += 1
 
-'''
-print adj_matrix
+    i += 1
 
-nodes_not_visited = []
-count = 1
-while(count <= n):
-    nodes_not_visited.append(count)
-    count +=1
-
-
-
-print nodes_not_visited
-
-'''
-'''
-
-nodes_visited  = np.zeros(n)
-nodes_visited[0] = 1
-
-i = 1
-distance_graph = np.empty(n)
-
-while ( i < n):
-    distance_graph[n]= adj_matrix[1][i]
+i = 0
+for i< n:
+    print "Vertex: " + str(i+1) + " Distance from source: " + str(dist[i])
     i+=1
-
-
-distance_graph = np.empty(n)
-distance_graph.fill(100000)
-distance_graph[0] = 0
-
-'''
